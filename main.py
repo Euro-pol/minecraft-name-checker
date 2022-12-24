a648ddffd276f98979457ad22ff368c232e1055c
timof121 <52547763+timof121@users.noreply.github.com>
2022-12-24 12:54:16 +0100

    best

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from colorama import Fore
 
 client = httpx.Client()
 delay = float(input("Delay between checks (seconds): "))
+choice = input("What do you want to do? (1) Check random words (2) Check from words.txt: ")
 
 def add_name(name):
     with open("names.txt", "a") as f:
@@ -15,18 +16,41 @@ def check(name):
         return True
     elif r.status_code == 200:
         return False    
+    elif r.status_code == 429:
+        print(Fore.YELLOW + "You are being ratelimited!")
+        time.sleep(5)
+        return False    
     else:
         return False
 
 def checkWord():
-    word = client.get("https://random-word-api.herokuapp.com/word").json()[0]
-    if(check(word)):
-        print(Fore.GREEN + f"{word} is available!")
-        add_name(word)
+    while True:
+        word = client.get("https://random-word-api.herokuapp.com/word").json()[0]
+        if(check(word)):
+            print(Fore.GREEN + f"{word} is available!")
+            add_name(word)
+        else:
+            print(Fore.RED + f"{word} is not available!")
+        time.sleep(delay)    
+
+def checkFile():
+    with open("words.txt", "r") as f:
+        words = f.read().splitlines()
+    for word in words:
+        if(check(word)):
+            print(Fore.GREEN + f"{word} is available!")
+            add_name(word)
+        else:
+            print(Fore.RED + f"{word} is not available!")
+        time.sleep(delay)
+
+
+
+if __name__ == "__main__":
+    if choice == "1":
+        checkWord()
+    elif choice == "2":
+        checkFile()
     else:
-        print(Fore.RED + f"{word} is not available!")
-
-
-while True:
-    checkWord()
-    time.sleep(delay)
+        print("Invalid choice!")
+    
